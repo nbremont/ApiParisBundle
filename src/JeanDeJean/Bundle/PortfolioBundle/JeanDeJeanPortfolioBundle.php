35c8d24f9e5f973961c3f3bdd9c5c9c877a55a5e
nbremont <bremon_a@etna-alternance.net>
2013-12-30 11:37:37 +0100

    remove old file

--- src/JeanDeJean/Bundle/PortfolioBundle/JeanDeJeanPortfolioBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace JeanDeJean\Bundle\PortfolioBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class JeanDeJeanPortfolioBundle extends Bundle
-{
-}
--- src/JeanDeJean/Bundle/PortfolioBundle/Tests/Controller/AdminPageControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace JeanDeJean\Bundle\PortfolioBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class AdminPageControllerTest extends WebTestCase
-{
-}
