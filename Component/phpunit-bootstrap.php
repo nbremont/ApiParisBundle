6ae436e8f5a1b00999b542bad404751f14689ce1
nbremont <bremon_a@etna-alternance.net>
2014-01-09 14:36:38 +0100

    update directory

--- Component/phpunit-bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
